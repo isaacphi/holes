a3af12cdfee6d06890d3f7200a8bc26ea1d7148f
phil <isaac.phil@gmail.com>
2022-04-10 15:58:25 -0400

    clean up

--- src/main.rs
+++ src/main.rs
@@ -10,26 +10,13 @@ use stm32f3_discovery::stm32f3xx_hal::prelude::*;
 use stm32f3_discovery::stm32f3xx_hal::pac;
 use stm32f3_discovery::stm32f3xx_hal::delay::Delay;
 
-// use stm32f3_discovery::button::UserButton;
-use stm32f3_discovery::switch_hal::{Switch, IntoSwitch, InputSwitch, OutputSwitch};
+use stm32f3_discovery::switch_hal::{IntoSwitch, InputSwitch, OutputSwitch};
 
 #[entry]
 fn main() -> ! {
-
+    // Configure peripherals
     let device_periphs = pac::Peripherals::take().unwrap();
     let mut reset_control_clock = device_periphs.RCC.constrain();
-    let mut gpioe = device_periphs.GPIOE.split(&mut reset_control_clock.ahb);
-
-    let mut led =
-            gpioe
-            .pe12
-            .into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper)
-            .into_active_high_switch();
-    led.off().unwrap();
-
-    // initialize user button
-    let mut gpioa = device_periphs.GPIOA.split(&mut reset_control_clock.ahb);
-    let mut button = gpioa.pa2.into_pull_up_input(&mut gpioa.moder, &mut gpioa.pupdr).into_active_low_switch();
 
     let mut core_periphs = cortex_m::Peripherals::take().unwrap();
     let stim = &mut core_periphs.ITM.stim[0];
@@ -37,12 +24,23 @@ fn main() -> ! {
     let clocks = reset_control_clock.cfgr.freeze(&mut flash.acr);
     let mut delay = Delay::new(core_periphs.SYST, clocks);
 
+    // LED
+    let mut gpioe = device_periphs.GPIOE.split(&mut reset_control_clock.ahb);
+    let mut led =
+            gpioe
+            .pe12
+            .into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper)
+            .into_active_high_switch();
+    led.off().unwrap();
+
+    // Start button
+    let mut gpioa = device_periphs.GPIOA.split(&mut reset_control_clock.ahb);
+    let button = gpioa.pa2.into_pull_up_input(&mut gpioa.moder, &mut gpioa.pupdr).into_active_low_switch();
+
     iprintln!(stim, "Begin!");
 
     loop {
-        // led.toggle().unwrap();
-        // delay.delay_ms(1000u16);
-        delay.delay_ms(200u16);
+        delay.delay_ms(50u16);
         match button.is_active() {
             Ok(true) => {
                 iprintln!(stim, "on");
