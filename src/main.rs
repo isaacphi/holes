474d94dc9348fdefbbb485d77c7225603f2c75d2
phil <isaac.phil@gmail.com>
2022-04-11 21:52:24 -0400

    pwm

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ use cortex_m::{iprintln};
 use stm32f3_discovery::stm32f3xx_hal::prelude::*;
 use stm32f3_discovery::stm32f3xx_hal::pac;
 use stm32f3_discovery::stm32f3xx_hal::delay::Delay;
+use stm32f3_discovery::stm32f3xx_hal::{pwm::tim1, time::rate::*};
 
 use stm32f3_discovery::switch_hal::{IntoSwitch, InputSwitch, OutputSwitch};
 
@@ -16,25 +17,30 @@ use stm32f3_discovery::switch_hal::{IntoSwitch, InputSwitch, OutputSwitch};
 fn main() -> ! {
     // Configure peripherals
     let device_periphs = pac::Peripherals::take().unwrap();
-    let mut reset_control_clock = device_periphs.RCC.constrain();
+    let mut rcc = device_periphs.RCC.constrain();
 
     let mut core_periphs = cortex_m::Peripherals::take().unwrap();
     let stim = &mut core_periphs.ITM.stim[0];
     let mut flash = device_periphs.FLASH.constrain();
-    let clocks = reset_control_clock.cfgr.freeze(&mut flash.acr);
+    let clocks = rcc.cfgr.freeze(&mut flash.acr);
     let mut delay = Delay::new(core_periphs.SYST, clocks);
 
+    // PWM
+    // Set the resolution of our duty cycle to 9000 and our period to
+    // 50Hz.
+    let mut gpioa = device_periphs.GPIOA.split(&mut rcc.ahb);
+    let (mut c1_no_pins, _, _, _) = tim1(device_periphs.TIM1, 9000, 50.Hz(), &clocks);
+    let pa8 = gpioa.pa8.into_af6_push_pull(&mut gpioa.moder, &mut gpioa.otyper, &mut gpioa.afrh);
+    let mut ch1 = c1_no_pins.output_to_pa8(pa8);
+    ch1.set_duty(ch1.get_max_duty() / 2);
+    ch1.enable();
+    
     // LED
-    let mut gpioe = device_periphs.GPIOE.split(&mut reset_control_clock.ahb);
-    let mut led =
-            gpioe
-            .pe12
-            .into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper)
-            .into_active_high_switch();
+    let mut gpioe = device_periphs.GPIOE.split(&mut rcc.ahb);
+    let mut led = gpioe.pe12.into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper).into_active_high_switch();
     led.off().unwrap();
 
     // Start button
-    let mut gpioa = device_periphs.GPIOA.split(&mut reset_control_clock.ahb);
     let button = gpioa.pa2.into_pull_up_input(&mut gpioa.moder, &mut gpioa.pupdr).into_active_low_switch();
 
     iprintln!(stim, "Begin!");
@@ -51,7 +57,7 @@ fn main() -> ! {
                 led.off().ok();
             }
             Err(_) => {
-                panic!("Failed to read button state");
+                panic!("Failed to read start button state");
             }
         }
     }
